70706e5ed4f343e25c8f4129f34da2ba1890046c
Nikita FREJNEMAN <nikita.frejneman@soprasteria.com>
2021-01-12 10:46:19 +0100

    125: Impementation of the burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auz';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main class={classes.Content}>
             {props.children}
         </main>
     </Aux>
