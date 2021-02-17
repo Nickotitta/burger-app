58af417e2ff34b5320667008875b5199b915e6da
Nick Frejneman <nick.frejneman@gmail.com>
2021-02-17 18:23:36 +0100

    130-Calculating

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Burger from '../../components/Burger/Burger';
 import Aux from "../../hoc/Auz";
 
 class BurgerBuilder extends Component {
+   
+    state ={
+        ingredients:{
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+   }
+
         render() {
             return (
                 <Aux>
-                    <Burger />
+                    <Burger ingredients={this.state.ingredients}/>
                     <div>Build Controls</div>
                 </Aux>
             );
