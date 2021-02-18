2a6a037ac0188206d63325dc1cd2bcad538831b0
Nick Frejneman <nick.frejneman@gmail.com>
2021-02-18 20:56:10 +0100

    134-135 Remove ingredient and add price tag

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,16 +35,37 @@ class BurgerBuilder extends Component {
     this.setState({totalpPice: newPrice, ingredients: updatedIngredients});
    }
 
-   removeIngredienthandler = (type) => {
-
+   removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    if(oldCount <= 0){
+        return;
+    }
+    const updatedCount = oldCount -1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    }; 
+    updatedIngredients[type] = updatedCount; 
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice  = this.state.totalpPice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalpPice: newPrice, ingredients: updatedIngredients});
    }
 
         render() {
+            const disabledInfo ={
+                ...this.state.ingredients
+            };
+            for(let key in disabledInfo){
+                disabledInfo[key] = disabledInfo[key] <= 0
+            }
             return (
                 <Aux>
                     <Burger ingredients={this.state.ingredients}/>
                     <BuildControls 
-                        ingredientAdded={this.addingredientHandler}/>
+                        ingredientAdded={this.addingredientHandler}
+                        ingredientRemoved={this.removeIngredientHandler}
+                        disabled={disabledInfo}
+                        price={this.state.totalpPice}/>
                 </Aux>
             );
         }
